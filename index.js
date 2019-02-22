84423c19adeccf8cc3fb509c09cf2d923e1add51
hejia <hejia@codoon.com>
2019-02-22 15:02:59 +0800

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+import { parseTime, createTimeString } from './lib/parseTime.js'
+import { wxAlert, wxToast, wxCheckNewVersRload, compareVersion } from './lib/wxTool.js'
+export default {
+  parseTime,
+  createTimeString,
+  wxAlert,
+  wxToast,
+  wxCheckNewVersRload,
+  compareVersion
+}
